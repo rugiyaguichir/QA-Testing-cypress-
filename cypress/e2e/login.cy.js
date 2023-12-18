6d202e513b7f0efb571e7fae608d50c04b4b153d
Rugiya <rugiyagujcir@gmail.com>
2023-12-18 20:59:49 +0600

    *

--- cypress/e2e/login.cy.js
+++ cypress/e2e/login.cy.js
@@ -1,9 +1,28 @@
 describe('Login Suite:', () => {
-  it('should do login 0', () => {
+  it('should do login successfully', () => {
     cy.visit('http://167.114.201.175:5000/');
     cy.get('input[name="userName"]').type('Admin');
     cy.get('input[name="password"]').type('Admin@Navi');
     cy.get('button[type="submit"]').click();
-    cy.pause();
+    cy.wait(2000);
+    cy.url.should('include', 'clients');
+  })
+
+  it('should check validations: invalid Login', ()=> {
+    cy.visit('http://167.114.201.175:5000/');
+    cy.get('input[name="userName"]').type('InvalidLogin');
+    cy.get('input[name="password"]').type('Admin@Navi');
+    cy.get('button[type="submit"]').click();
+    cy.wait(2000);
+    cy.url.should('include', 'login');
+  })
+
+  it('should check validations: invalid Password', ()=> {
+    cy.visit('http://167.114.201.175:5000/');
+    cy.get('input[name="userName"]').type('Admin');
+    cy.get('input[name="password"]').type('Admin@InvalidPassword');
+    cy.get('button[type="submit"]').click();
+    cy.wait(2000);
+    cy.url.should('include', 'login');
   })
 })
